f9e4602bdfc5ec9c998b0aa51f84f4e777563a8b
Richu <richujain1995@gmail.com>
2022-03-21 10:33:12 -0400

    Refactored express functions to use async and await.

--- src/index.js
+++ src/index.js
@@ -9,22 +9,39 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) =>{
+
+    try{
+        await user.save()
+        // The code here runs only if the above code runs successfully without any error. 
+        res.status(201).send(user)
+    } catch (e){
         res.status(400).send(e)
-        //res.send(e)
-    })
+    }
+
+    // user.save().then(() => {
+    //     res.send(user)
+    // }).catch((e) =>{
+    //     res.status(400).send(e)
+    //     //res.send(e)
+    // })
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    
+    try{
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch (e){
         res.status(500).send()
-    })
+    }
+
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
       
 })
 
@@ -32,47 +49,85 @@ app.get('/users', (req, res) => {
 
 // Express provides route parameters to catch the parameters passed through the url.
 //req.params contains all the parameters we add. In this case, it is an object with a single property, ie, id
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+
+    try{
+        const user = await User.findById(_id)
         if (!user){
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send()
-    })
+    }
+
+    // User.findById(_id).then((user) => {
+    //     if (!user){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-    task.save().then(() => {
-        res.send(task)
-    }).catch((e) => {
+
+    try{
+        await task.save()
+        res.status(201).send(task)
+    } catch(e){
         res.status(400).send(e)
-    })
+    }
+
+    // task.save().then(() => {
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
+app.get('/tasks', async (req, res) => {
+
+    try{
+        const tasks = await Task.find({})
+        res.status(201).send(tasks)
+    } catch(e){
         res.status(500).send()
-    })
+    }
+
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
+
+    try{
+        const task = await Task.findById(_id)
         if(!task){
             return res.status(404).send()
         }
         res.send(task)
-    }).catch((e) => {
+    } catch(e){
         res.status(500).send()
-    })
+    }
+    
+    // Task.findById(_id).then((task) => {
+    //     if(!task){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 app.listen(port, () => {
