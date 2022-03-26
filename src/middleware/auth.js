ddad3bb63b03bd765a30b77dfaefd8f325eea7cf
Richu <richujain1995@gmail.com>
2022-03-26 11:51:55 -0400

    Middleware for /users/me router for showing logged user details by verifying JWT.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const auth = async (req, res, next) => {
-    console.log('auth middleware')
-    next()
+    try{
+        const token = req.header('Authorization').replace('Bearer ', '') //replace to remove Bearer word from token
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const user = await User.find({ _id: decoded._id, 'tokens.token': token }) //This function will find the user with the ID and the token we passed. 
+        
+        if(!user){
+            throw new Error()
+        }
+        req.user = user
+        next()
+
+    } catch(e){
+        res.status(401).send({ error: 'Please authenticate.'})
+    }
 }
 
 module.exports = auth
--- src/routers/user.js
+++ src/routers/user.js
@@ -35,14 +35,16 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
+
+    res.send(req.user)
     
-    try{
-        const users = await User.find({})
-        res.send(users)
-    } catch (e){
-        res.status(500).send()
-    }
+    // try{
+    //     const users = await User.find({})
+    //     res.send(users)
+    // } catch (e){
+    //     res.status(500).send()
+    // }
 
     // User.find({}).then((users) => {
     //     res.send(users)
