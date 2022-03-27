7b9ac125f962a89474c90f4b9dbddbd51718007c
Richu <richujain1995@gmail.com>
2022-03-26 22:31:36 -0400

    Logout API to remove current token and logoutAll router to remove all tokens.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,15 +5,18 @@ const auth = async (req, res, next) => {
     try{
         const token = req.header('Authorization').replace('Bearer ', '') //replace to remove Bearer word from token
         const decoded = jwt.verify(token, 'thisismynewcourse')
-        const user = await User.find({ _id: decoded._id, 'tokens.token': token }) //This function will find the user with the ID and the token we passed. 
-        
+        //This function will find the user with the ID and the token we passed. 
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }) 
+        console.log('User is: '+ user)
         if(!user){
             throw new Error()
         }
+        req.token = token
         req.user = user
         next()
 
     } catch(e){
+        console.log('a' + e)
         res.status(401).send({ error: 'Please authenticate.'})
     }
 }
