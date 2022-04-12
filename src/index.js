9a91d89bc67e5aa6a478f9f971a8da5ba2929490
Richu <richujain1995@gmail.com>
2022-04-12 11:25:44 -0400

    Regex to add restrictions in filetyle and also added fileSize limit in multer object.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+        // There are other limits also we can use. 
+    },
+    fileFilter(req, file, cb) {
+        // if (!file.originalname.endsWith('.pdf')) {
+        //     return cb(new Error('Please upload a PDF'))
+        // }
+        // Google Slide 37
+        if (!file.originalname.match(/\.(doc|docx)$/)) { // Regular expressions are written inside forward slashes
+            return cb(new Error('Please upload a Word document'))
+        }
+        cb(undefined, true)
+        // cb(new Error('File must be a PDF')) // Reject upload with error
+        // cb(undefined, true) // Upload Success
+        // cb(undefined, false) // Reject upload silently
+    }
 })
 app.post('/upload', upload.single('upload'), (req,res) => {
     res.send()
