e4ce5877652fafce7aa42ce090f48b0a5f3c740e
Richu <richujain1995@gmail.com>
2022-04-28 12:42:21 -0400

    Cleaned up project directory. Removed images and avatar directory as we are directly uploading images to Mangodb.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const e = require('express')
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
@@ -7,27 +6,27 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-        // There are other limits also we can use. 
-    },
-    fileFilter(req, file, cb) {
-        // if (!file.originalname.endsWith('.pdf')) {
-        //     return cb(new Error('Please upload a PDF'))
-        // }
-        // Google Slide 37
-        if (!file.originalname.match(/\.(doc|docx)$/)) { // Regular expressions are written inside forward slashes
-            return cb(new Error('Please upload a Word document'))
-        }
-        cb(undefined, true)
-        // cb(new Error('File must be a PDF')) // Reject upload with error
-        // cb(undefined, true) // Upload Success
-        // cb(undefined, false) // Reject upload silently
-    }
-})
+// const multer = require('multer')
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//         // There are other limits also we can use. 
+//     },
+//     fileFilter(req, file, cb) {
+//         // if (!file.originalname.endsWith('.pdf')) {
+//         //     return cb(new Error('Please upload a PDF'))
+//         // }
+//         // Google Slide 37
+//         if (!file.originalname.match(/\.(doc|docx)$/)) { // Regular expressions are written inside forward slashes
+//             return cb(new Error('Please upload a Word document'))
+//         }
+//         cb(undefined, true)
+//         // cb(new Error('File must be a PDF')) // Reject upload with error
+//         // cb(undefined, true) // Upload Success
+//         // cb(undefined, false) // Reject upload silently
+//     }
+// })
 // Commenting for referance. Removing the middleware to add custom middleware to return JSON error instead of the default HTML error
 // app.post('/upload', upload.single('upload'), (req,res) => {
 //     res.send()
@@ -42,19 +41,15 @@ const upload = multer({
 // }, (error, req, res, next) => {
 //     res.status(400).send({ error: error.message })
 // })
-const errorMiddleware = (req, res, next) => {
-    throw new Error('From my middleware')
-}
-// It is required to pass all four arguments to let the express know that it's the function to be executed when it catches an error. The four arguments are error, req, res, next
-app.post('/upload', upload.single('upload'), (req,res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
-
-
-
+// const errorMiddleware = (req, res, next) => {
+//     throw new Error('From my middleware')
+// }
+// // It is required to pass all four arguments to let the express know that it's the function to be executed when it catches an error. The four arguments are error, req, res, next
+// app.post('/upload', upload.single('upload'), (req,res) => {
+//     res.send()
+// }, (error, req, res, next) => {
+//     res.status(400).send({ error: error.message })
+// })
 
 app.use(express.json())
 app.use(userRouter)
@@ -64,14 +59,8 @@ app.listen(port, () => {
     console.log('Server is up on port '+ port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-
-
-
-
-
+// const Task = require('./models/task')
+// const User = require('./models/user')
 
 // app.use((req, res, next) => {
 //     if(req.method === 'GET'){
